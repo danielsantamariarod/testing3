ac1e074fa6463a67db75373b9bd89945008b9a69
danielsantamariarod <danielsantamariarod@uniandes.edu.co>
2025-05-20 17:01:14 -0500

    Update tests.R
    first test

--- tests.R
+++ tests.R
@@ -1,11 +1,21 @@
 # Tests.R
 # Archivo de pruebas
-test_trial1 <- function() {
-	  if (estado == "aprobado") {
-	  cat("El estudiante ha aprobado el curso.\n")
-	} else if (estado == "reprobado") {
-	  cat("El estudiante ha reprobado el curso.\n")
+test_trial1 <- function(gastos_mayores_transporte) {
+	# Ciudades esperadas
+	ciudades_esperadas <- c("Armenia", "Bogotá", "Bucaramanga y A.M.", "Cali", "Manizales y A.M.", "Medellín y A.M.")
+	
+	# Verificación
+	if (all(c("Ciudad", "Transporte") %in% colnames(gastos_mayores_transporte))) {
+	  if (nrow(gastos_mayores_transporte) == 6) {
+	    if (all(gastos_mayores_transporte$Ciudad %in% ciudades_esperadas)) {
+	      cat("La variable cumple con todas las condiciones.\n")
+	    } else {
+	      cat("Los valores de la columna 'Ciudad' no coinciden con los esperados.\n")
+	    }
+	  } else {
+	    cat("La variable no tiene exactamente 6 filas.\n")
+	  }
 	} else {
-	  cat("Estado no reconocido.\n")
+	  cat("La variable no tiene las columnas requeridas.\n")
 	}
 }
